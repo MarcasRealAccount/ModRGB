b5bea6b280bc5bd4e2c05ed13201fc02066b0a6c
MarcasRealAccount <thetremine.lumicks@gmail.com>
2022-04-12 18:02:02 +0200

    Make version constexpr safe

--- Libraries/ReliableUDP/Inc/ReliableUDP/Utils/Version.h
+++ Libraries/ReliableUDP/Inc/ReliableUDP/Utils/Version.h
@@ -9,26 +9,20 @@ namespace ReliableUDP::Utils
 	public:
 		constexpr Version() noexcept : m_Version(0U) {}
 		constexpr Version(std::uint32_t version) noexcept : m_Version(version) {}
-		constexpr Version(std::uint32_t major, std::uint32_t minor, std::uint32_t patch) noexcept : m_SubVersions(major, minor, patch) {}
+		constexpr Version(std::uint32_t major, std::uint32_t minor, std::uint32_t patch) noexcept : m_Version((major & 0x3FF) << 22 | (minor & 0x3FF) << 12 | patch & 0xFFF) {}
 
 		constexpr operator std::uint32_t() const noexcept { return m_Version; }
 
-		constexpr bool isCompatible(Version other) const noexcept { return !(other.m_SubVersions.major != m_SubVersions.major || other.m_SubVersions.minor > m_SubVersions.minor); }
+		constexpr std::uint16_t major() const noexcept { return (m_Version >> 22) & 0x3FF; }
+		constexpr std::uint16_t minor() const noexcept { return (m_Version >> 12) & 0x3FF; }
+		constexpr std::uint16_t patch() const noexcept { return m_Version & 0xFFF; }
+		constexpr void          major(std::uint16_t major) noexcept { m_Version = (m_Version & 0x3FFFFF) | (major & 0x3FF) << 22; }
+		constexpr void          minor(std::uint16_t minor) noexcept { m_Version = (m_Version & 0xFFC00FFF) | (minor & 0x3FF) << 12; }
+		constexpr void          patch(std::uint16_t patch) noexcept { m_Version = (m_Version & 0xFFFFF000) | (patch & 0xFFF); }
+
+		constexpr bool isCompatible(Version other) const noexcept { return !(other.major() != major() || other.minor() > minor()); }
 
 	public:
-		union
-		{
-			struct SubVersions
-			{
-			public:
-				constexpr SubVersions(std::uint32_t major, std::uint32_t minor, std::uint32_t patch) : patch(patch), minor(minor), major(major) {}
-
-			public:
-				std::uint32_t patch : 12;
-				std::uint32_t minor : 10;
-				std::uint32_t major : 10;
-			} m_SubVersions;
-			std::uint32_t m_Version;
-		};
+		std::uint32_t m_Version;
 	};
 } // namespace ReliableUDP::Utils
